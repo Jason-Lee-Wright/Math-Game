a5e4af7bb180ca54c1320dbeedc3e4ff78b8e80e
Jason <jason974448@gmail.com>
2024-11-25 11:56:11 -0400

    Impilmented a player movement, and camera controller

---
+++ Assets/Scripts/Camera Movement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject player;
+    public float distance = 5.0f;
+    public float xSpeed = 120.0f;
+    public float ySpeed = 120.0f;
+
+    private float x = 0.0f;
+    private float y = 0.0f;
+
+    void Start()
+    {
+        Vector3 angles = transform.eulerAngles;
+        x = angles.y;
+        y = angles.x;
+    }
+
+    void LateUpdate()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.02f;
+            y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+
+            y = Mathf.Clamp(y, -20, 80);
+        }
+        else
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        Quaternion rotation = Quaternion.Euler(y, x, 0);
+        Vector3 position = player.transform.position - rotation * Vector3.forward * distance;
+
+        transform.rotation = rotation;
+        transform.position = position;
+    }
+}
--- Assets/Scripts/Player Movement.cs
+++ Assets/Scripts/Player Movement.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 0;
+
+    public GameObject Camera;
+
+    private Rigidbody rb;
+
+    private float movementX;
+    private float movementY;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    private void FixedUpdate()
     {
         
+        Vector3 cameraForward = Camera.transform.forward;
+        Vector3 cameraRight = Camera.transform.right;
+
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+
+        Vector3 movement = cameraForward * movementY + cameraRight * movementX;
+
+        rb.AddForce(movement * speed);
     }
+
 }
+
